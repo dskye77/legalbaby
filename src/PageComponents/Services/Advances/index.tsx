269b419b48a4c56b0ded628a898d730f12d4df7c
dskyle77 <dskyle77@gmail.com>
2026-01-10 16:01:19 +0100

    l

--- src/PageComponents/Services/Advances/index.tsx
+++ src/PageComponents/Services/Advances/index.tsx
@@ -1,15 +1,11 @@
 import Hero from "./Hero";
 import Offer from "./Offer";
-import Approach from "./Approach";
-import Difference from "./Difference";
 
 export default function PlaylistsPage() {
   return (
     <div>
       <Hero />
       <Offer />
-      <Approach />
-      <Difference />
     </div>
   );
 }
